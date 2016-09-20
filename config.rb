9a3f96e746ee6cac0e9fb71f9695f092df2a4c84
pengcl <3559996@qq.com>
2016-09-20 11:58:56 +0800

    biuld angular-seed

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ Encoding.default_external = Encoding.find('utf-8')
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "app/css"
+css_dir = "public/css"
 sass_dir = "sass"
-images_dir = "app/images"
-javascripts_dir = "app/js"
+images_dir = "public/images"
+javascripts_dir = "public/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
