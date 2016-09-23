c968bc4747e398b5e819a1f01fc0f0e774a67695
pengcl <3559996@qq.com>
2016-09-24 01:30:28 +0800

    biuld angular-seed

--- public/html/pages/phone/phone-details/default/phone-details.js
+++ public/html/pages/phone/phone-details/default/phone-details.js
@@ -40,6 +40,9 @@ app.config(['$stateProvider', '$locationProvider', function ($stateProvider, $lo
 
                 //选择默认内存
                 $scope.storage = phone.storages[getIndex(phone.storages, "curr")];
+
+                $scope.phoneType = phone.phoneTypes[getIndex(phone.phoneTypes, "curr")];
+
                 $scope.mainPrice = phone.price;
             });
         }
