dfb00879df5354027e92f21df801473f45e31d72
pengcl <3559996@qq.com>
2016-11-17 17:47:35 +0800

    买一送一

---
+++ public/html/modules/payType/n/payType.js
@@ -0,0 +1,39 @@
+'use strict';
+
+app.directive("nPayType", ['$location', '$compile', '$q', function ($location, $compile, $q) {
+    return {
+        restrict: 'E',
+        templateUrl: "modules/payType/n/payType.html",
+        link: function (scope, element, attrs) {
+            //模块标题
+            scope.payTitle = attrs.title;
+            scope.paySubTitle = attrs.subTitle;
+            //设置本模块的显示隐藏
+            scope.visibility = attrs.visibility;
+            if (scope.visibility === "false") {
+                $(element).addClass("hide");
+            }
+
+            var $form = $(attrs.submit);
+
+            //默认赋值
+            //scope.payType = 1;
+
+            //设置默认payType;
+            scope.setDefaultPayType = function (id, typeName) {
+                scope.payType = id;
+                scope.payTypeName = name;
+            };
+
+            scope.setDefaultPayType(2,"信用卡分期");
+
+            //选择支付方式
+
+            scope.setPayType = function (event, id, typeName) {
+                event.preventDefault();
+                scope.payType = id;
+                scope.payTypeName = typeName;
+            };
+        }
+    };
+}]);
