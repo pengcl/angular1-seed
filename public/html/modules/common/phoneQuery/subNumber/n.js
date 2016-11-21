63f3a34d1e061dcfc773cd3b83d5b136bc5ca090
pengcl <3559996@qq.com>
2016-11-21 09:56:51 +0800

    修复主副卡可以选择相同号码的问题

--- public/html/modules/common/phoneQuery/subNumber/n.js
+++ public/html/modules/common/phoneQuery/subNumber/n.js
@@ -7,12 +7,14 @@ app.directive("subNumber", ["$cookieStore", function ($cookieStore) {
         controller: "numberController",
         link: function (scope, element, attrs) {
 
+            var $container = $('.content-scrollable');
+
             scope.checkSubNumber = function () {
                 //alert("sub");
                 if (!scope.checkoutForm.subNumber.$valid) {//原本应该用!scope.checkoutForm.phoneNumber.$valid
                     var $scrollTo = $('#pickSubNumber');
                     $container.animate({
-                        scrollTop: $scrollTo.offset().top - $container.offset().top + $container.scrollTop() -50
+                        scrollTop: $scrollTo.offset().top - $container.offset().top + $container.scrollTop() - 50
                     });
                     $("#pickSubNumberPanel").slideDown();
                     return false;
@@ -22,16 +24,16 @@ app.directive("subNumber", ["$cookieStore", function ($cookieStore) {
 
             scope.setSubNumber = function (event, numberItem) {
                 event.preventDefault();
-                scope.subNumber = numberItem.n;
-
-                //$("#pickSubNumberPanel").slideToggle();
-
                 var $this = $(event.currentTarget);
 
-                $this.parent().siblings().children().removeClass('curr');
-                $this.addClass('curr');
-
-                writebdLog(scope.category, "_subSelectNumber", "渠道号", scope.gh);//选择号码
+                if (checkSameNumber(scope.mainNumber,numberItem.n)) {
+                    scope.subNumber = numberItem.n;
+                    $this.parent().siblings().children().removeClass('curr');
+                    $this.addClass('curr');
+                    writebdLog(scope.category, "_subSelectNumber", "渠道号", scope.gh);//选择号码
+                } else {
+                    scope.$root.dialog.open('系统提示', '您选择的主卡号码和副卡号码相同，请重新选择');
+                }
             };
 
             scope.showSNumberPn = function (e) {
--- public/js/extends.js
+++ public/js/extends.js
@@ -339,18 +339,25 @@ $(function () {
     });
 });
 
-function getRandomName (){
-    var firstNames,lastNames;
-    firstNames = ["卢","钟","朱","彭","梁","卫","蒋","许","张","孔","沈","郑","赵","周","吕","韩","尤","秦","楮","李"];
-    lastNames = ["先生","女士"];
-    var fid = Math.round(Math.random()*19);
-    var lid = Math.round(Math.random()*1);
+function getRandomName() {
+    var firstNames, lastNames;
+    firstNames = ["卢", "钟", "朱", "彭", "梁", "卫", "蒋", "许", "张", "孔", "沈", "郑", "赵", "周", "吕", "韩", "尤", "秦", "楮", "李"];
+    lastNames = ["先生", "女士"];
+    var fid = Math.round(Math.random() * 19);
+    var lid = Math.round(Math.random() * 1);
     return firstNames[fid] + lastNames[lid];
 }
 
-function getRandomProduct (){
+function getRandomProduct() {
     var productName;
-    productName = ["iPhone 7","iPhone 7 Plus","VIVO X7","OPPO R9","小米5S","华为P9"];
-    var index = Math.round(Math.random()*5);
+    productName = ["iPhone 7", "iPhone 7 Plus", "VIVO X7", "OPPO R9", "小米5S", "华为P9"];
+    var index = Math.round(Math.random() * 5);
     return productName[index];
+}
+
+function checkSameNumber(number1, number2) {
+    if (number1 === number2) {
+        return false;
+    }
+    return true;
 }
