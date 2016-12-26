57b8b351236b1913683e5a6d9f3888b801526279
pengcl <3559996@qq.com>
2016-12-26 12:02:51 +0800

    屏蔽同城验货

--- public/html/pages/referrerSys/login/login.js
+++ public/html/pages/referrerSys/login/login.js
@@ -14,14 +14,22 @@ app.config(['$stateProvider', '$locationProvider', function ($stateProvider, $lo
     //$scope.$root.title = $scope.pageTitle;
     //console.log($scope.referrerForm.referrerNo);
     $scope.homeUrl = "http://app.yfq.cn/phone/A/index";
+    $scope.searchOrder = "http://m.yfq.cn/wap/customer/searchIndexA.html";
+    $scope.$root.dialogClass = "referrer-dialog";
+
+    $scope.$root.hasMoreBtn = true;
+
     $scope.login = function (event, referrerNo) {
         if ($scope.referrerForm.referrerNo.$valid) {
-            $http.jsonp('http://192.168.1.181:8082/product/getQrCode.html?referrerNo=' + referrerNo + '&gh=cfps&activity=cfps&url=http://www.cz.com&callback=JSON_CALLBACK').success(function (data, status, headers, config) {
-                console.log(data[0].upCodePath.replace("m.gd189fq.com", "192.168.1.181:8082"));
-                var html = "<div class='img-box'><img src='" + data[0].upCodePath.replace("m.gd189fq.com", "192.168.1.181:8082") + "'></div>";
-                $scope.dialog.open("", html);
-                $("#js-dialog").find(".weui-dialog__btn").html("直接进入");
-                $("#js-dialog").find(".weui-dialog__btn").attr("href", $scope.homeUrl + "?gh=cfps&activity=cfps&referrerNo=" + referrerNo);
+            $http.jsonp('http://m.yfq.cn/product/getQrCode.html?referrerNo=' + referrerNo + '&gh=cfps&activity=cfps&url=' + $scope.homeUrl + '&callback=JSON_CALLBACK').success(function (data, status, headers, config) {
+                var html = "<div class='img-box'><img src='" + data[0].upCodePath + "'></div><p><a href='" + $scope.homeUrl + "?gh=cfps&activity=cfps&referrerNo=" + referrerNo + "'>进入官网</a></p>";
+                $http.jsonp('http://m.yfq.cn/product/getQrCode.html?referrerNo=' + referrerNo + '&gh=cfps&activity=cfps&url=' + $scope.searchOrder + '&callback=JSON_CALLBACK').success(function (data, status, headers, config) {
+                    html = html + "<div class='img-box'><img src='" + data[0].upCodePath + "'></div><p><a href='" + $scope.searchOrder + "?gh=cfps&activity=cfps&referrerNo=" + referrerNo + "'>查询订单</a></p>";
+                    $scope.dialog.open("", html);
+                }).error(function (data, status, headers, config) {
+                    console.log("error");
+                    //deferred.reject(status)
+                });
             }).error(function (data, status, headers, config) {
                 console.log("error");
                 //deferred.reject(status)
