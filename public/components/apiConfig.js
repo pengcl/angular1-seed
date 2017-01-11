0a8e167a0beec129547581215c9873701a13839c
pengcl <3559996@qq.com>
2017-01-11 18:31:30 +0800

    添加api配置项

--- public/components/apiConfig.js
+++ public/components/apiConfig.js
@@ -2,6 +2,6 @@
 var nodeApiHost = "http://app.yfq.cn:3099";*/
 
 var cfApi = {
-    apiHost: "http://192.168.1.181:8082",
+    apiHost: "http://m.yfq.cn",
     nodeApiHost: "http://app.yfq.cn:3099"
 };
