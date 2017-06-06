099ac97c8e0286a09d3e0e07242cf17a7477a983
ForestWithLin <841056253@qq.com>
2017-06-06 11:12:57 +0800

    lyhtcsj

--- public/html/pages/phoneCard/upgrade/index.js
+++ public/html/pages/phoneCard/upgrade/index.js
@@ -13,8 +13,8 @@ app.config(['$stateProvider', '$locationProvider', function ($stateProvider, $lo
     //$scope.$root.title = $scope.pageTitle;
     //console.log($scope.referrerForm.referrerNo);
 
-    $scope.pageType = "pcdUpgrade";
-    $scope.appType = systemName + "_upgrade_" + $scope.pageType;
+    $scope.pageType = "A";
+    $scope.appType = systemName + "_lyhtcsj_" + $scope.pageType;
     $scope.category = $scope.appType;
 
     $scope.params = window.location.search;
@@ -96,15 +96,7 @@ app.config(['$stateProvider', '$locationProvider', function ($stateProvider, $lo
             return false;
         }
 
-        var name = "";
-        if (product.name == "充 100 元送 5 元") name = "Give5Y";
-        if (product.name == "充 50 元送 30 元") name = "Give30Y";
-        if (product.name == "充 50 元送 50 元") name = "Give50Y";
-        if (product.name == "充 100 元送 100 元") name = "Give100Y";
-        if (product.name == "充 100 元") name = "Charge100Y";
-        if (product.name == "充 200 元送 150 元") name = "Give150Y";
-        if (product.name == "充 500 元送 300 元") name = "Give300Y";
-        writebdLog($scope.category, "_" + name, "渠道号", $scope.gh);
+        writebdLog($scope.category, "_UpgradePackages", "渠道号", $scope.gh);
 
         $scope.product = product;
     };
@@ -159,7 +151,7 @@ app.config(['$stateProvider', '$locationProvider', function ($stateProvider, $lo
         });
     };
 
-    $scope.submitForm = function (e, value) {
+    $scope.submitForm = function (e) {
         var $form = $("#checkoutForm");
 
         if (!$scope.checkoutForm.iccid.$valid) {
@@ -186,7 +178,7 @@ app.config(['$stateProvider', '$locationProvider', function ($stateProvider, $lo
                 $scope.$root.toast.close();
                 console.log();
                 window.location.href = data.payUrl;
-                writebdLog($scope.category, "_" + value, "渠道号", $scope.gh);//立即支付
+                writebdLog($scope.category, "_BuyNow", "渠道号", $scope.gh);//立即支付
             }
         });
 
