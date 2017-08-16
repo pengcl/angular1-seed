ec77e8b314bb63c58e1b3d8c5555bc93c4f7e257
pengcl <3559996@qq.com>
2017-08-16 17:16:53 +0800

    category

--- public/html/modules/gh/gh.js
+++ public/html/modules/gh/gh.js
@@ -1,19 +1,28 @@
 'use strict';
 
-app.directive("gh", ['$location', function ($location) {
+app.directive("gh", ['$location', '$cookies', function ($location, $cookies) {
     return {
         restrict: 'E',
         templateUrl: "modules/gh/gh.html",
         link: function (scope, element, attrs) {
+
+            var dt = new Date();
+            dt.setDate(dt.getDate() + 30);
+
+            scope.gh = "";
+
             if ($location.search().gh == undefined) {
-                scope.gh = "";
+                if ($cookies.get('app_gh')) {
+                    scope.gh = $cookies.get('app_gh');
+                }
             } else {
                 scope.gh = $location.search().gh;
+                $cookies.put('app_gh', scope.gh, {expires: dt});
             }
 
-            if($location.search().referrerNo == undefined){
+            if ($location.search().referrerNo == undefined) {
                 scope.referrerNo = "";
-            }else {
+            } else {
                 scope.referrerNo = $location.search().referrerNo;
             }
         }
